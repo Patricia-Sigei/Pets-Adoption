0943c521d608535a44698cf10b68de3c47fbfa44
BETH WACEKE KIMANI <kbwaceke@gmail.com>
2025-02-25 16:37:27 +0300

    added changes to images on landing page and css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import '../App.css';
 import BookingModal from './BookingPet'; 
-import mainImage from '../assets/images/homepg.jpg'; 
 
 const Hero = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,10 +15,8 @@ const Hero = () => {
 
   return (
     <div className="hero">
-      <img src={mainImage} alt="Dog Adoption" className="hero-image" />
       <div className="hero-text">
         <h1 className="hero-title"> Adopt a Pet and Get a Lifetime Companion</h1>
-       
         <button className="btn" onClick={openModal}>Adopt Now</button>
       </div>
       {isModalOpen && <BookingModal onClose={closeModal} />}
