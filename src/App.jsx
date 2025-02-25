144730dca26764a347198f4d2a16b7d840e0b5cc
BETH WACEKE KIMANI <kbwaceke@gmail.com>
2025-02-25 16:59:01 +0300

    made one single page

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Hero from './components/Hero'; 
+import AboutUs from './components/AboutUs'; 
+import Footer from './components/Footer';
 import HomePage from './components/HomePage'; 
 import SearchPage from "./components/SearchPage"; 
 import ErrorBoundary from "./components/ErrorBoundary"; 
 import LiveChat from './components/LiveChat';
 import SearchAndDisplayPets from './components/Display'; 
 import Header from './components/Header';
-import AboutUs from './components/AboutUs';
-import Footer from './components/Footer';
 
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <AboutUs />
+      <Footer />
+    </>
+  );
+};
 
 function App() {
   return (
@@ -23,15 +32,13 @@ function App() {
           <Navbar />
           
           <Routes>
-            <Route path="/" element={<Hero />} /> {/* Landing Page */}
+            <Route path="/" element={<Home />} /> {/* Home with Hero, About, Footer */}
             <Route path="/catalogue" element={<HomePage />} /> {/* Catalogue Page */}
             <Route path="/search" element={<SearchPage />} /> {/* Search Page */}
             <Route path="/search-and-display" element={<SearchAndDisplayPets />} /> {/* Search and Display Pets */}
             <Route path="/about" element={<AboutUs />} /> {/* About Us as its own page */}
-          
           </Routes>
           
-          <Footer />
           <LiveChat />
         </ErrorBoundary>
       </Router>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import './Footer.css';
 
 const Footer = () => {
-    const location = useLocation();
-
-    // Show the footer only on the About Us page
-    if (location.pathname !== "/about") return null;
-
     return (
         <footer className="footer">
             <div className="footer-container">
-                {/* Contact Section */}
                 <div className="footer-section">
                     <h5>Contact Us</h5>
                     <p><FontAwesomeIcon icon={faPhoneAlt} /> +254 700 123 456</p>
                     <p><FontAwesomeIcon icon={faEnvelope} /> adopt@petadoptionagency.com</p>
                 </div>
 
-                {/* Adoption Information */}
                 <div className="footer-section">
                     <h5>Adoption Info</h5>
                     <ul>
@@ -32,7 +24,6 @@ const Footer = () => {
                     </ul>
                 </div>
 
-                {/* Newsletter Section */}
                 <div className="footer-section">
                     <h5>Newsletter</h5>
                     <p>Subscribe for pet adoption updates.</p>
@@ -43,7 +34,6 @@ const Footer = () => {
                     </form>
                 </div>
 
-                {/* Our Socials Section */}
                 <div className="footer-socials">
                     <h5>Our Socials</h5>
                     <div className="social-icons">
@@ -54,7 +44,6 @@ const Footer = () => {
                 </div>
             </div>
 
-            {/* Footer Bottom */}
             <div className="footer-bottom">
                 <p>&copy; {new Date().getFullYear()} Pet Adoption Agency. All Rights Reserved.</p>
             </div>
